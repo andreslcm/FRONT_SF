97325a271d35629899c933f8aa8186f9bf312f4e
Andrés <trece920@hotmail.com>
2019-10-25 20:07:06 -0300

    definición de paths

--- src/router.js
+++ src/router.js
@@ -1,12 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LandingPage from './components/LandingPage.vue'
+import Logged from './components/Logged.vue'
+import Login from './components/Login.vue'
+import Registro from './components/Registro.vue'
 
 Vue.use(VueRouter)
 /**
  * Router para la navegación de la vista principal de la aplicación.
  */
 const routes =[
-
+  {
+    path: '/',
+    name: 'landing',
+    component: LandingPage
+  },
+  {
+    path: '/logged',
+    name: 'logged',
+    component: Logged
+  },
+  {
+    path: 'login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/registro',
+    name: 'registro',
+    component: Registro
+  }
 ]
 
 export const router = new VueRouter ({
