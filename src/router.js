0faa7e986c9456aad9c01e46455fd16909b2bf56
Andrés <trece920@hotmail.com>
2019-10-25 17:41:32 -0300

    creación del router y del store

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+/**
+ * Router para la navegación de la vista principal de la aplicación.
+ */
+const routes =[
+
+]
+
+export const router = new VueRouter ({
+  routes,
+  mode: 'history'
+})
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import { router } from './router'
+
+Vue.use(Vuex)
+/**
+ * En el store se almacenarán los datos y los métodos para cambiar estos datos.
+ */
+export const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
