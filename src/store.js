ca66a9b3dbd5b41982f12327656f5f4e69ccf16f
Andrés <trece920@hotmail.com>
2019-10-27 16:39:31 -0300

    Función asincrónica para cargar clientes

--- src/store.js
+++ src/store.js
@@ -8,139 +8,158 @@ Vue.use(Vuex)
  * En el store se almacenan los datos y los métodos para cambiar estos datos.
  */
 export const store = new Vuex.Store({
-    state: {
-        token: localStorage.getItem("tokenJwt") || null,
-        id: localStorage.getItem("idUsuario") || null,
-        clientes: [],
-        facturas: [],
-        datosUsuario: {},
-        contador: 0,
+  state: {
+    token: localStorage.getItem("tokenJwt") || null,
+    id: localStorage.getItem("idUsuario") || null,
+    clientes: [],
+    facturas: [],
+    datosUsuario: {},
+    contador: 0,
+  },
+  mutations: {
+    /**
+     * Mutación para cambiar el estado del token.
+     * @param {estado} state 
+     * @param {String} token 
+     */
+    SET_TOKEN(state, token) {
+      state.token = token;
     },
-    mutations: {
-        /**
-         * Mutación para cambiar el estado del token.
-         * @param {estado} state 
-         * @param {String} token 
-         */
-        SET_TOKEN(state, token) {
-            state.token = token;
-        },
-        /**
-         * Mutación para cambiar el estado del id.
-         * @param {estado} state 
-         * @param {Number} id 
-         */
-        SET_ID(state, id) {
-            state.id = id;
-        },
-        /**
-         * Mutación para cambiar el estado del array de clientes.
-         * @param {estado} state 
-         * @param {Array obejtos cliente} clientes 
-         */
-        SET_CLIENTES(state, clientes) {
-            state.clientes = clientes;
-        },
-       /**
-         * Mutación para cambiar el estado del array de facturas.
-         * @param {estado} state 
-         * @param {Array obejtos factura} facturas 
-         */
-        SET_FACTURAS(state, facturas) {
-            state.facturas = facturas;
-        },
-         /**
-         * Mutación para cambiar el estado del contador.
-         * @param {estado} state 
-         * @param {Number} numero 
-         */
-        SET_CONTADOR(state, numero) {
-            state.contador = numero;
-        },
-         /**
-         * Mutación para cambiar el estado del array de clientes.
-         * @param {estado} state 
-         * @param {Objeto usuario} datos 
-         */
-        SET_DATOS_USUARIO(state, datos){
-          state.datosUsuario = datos;
-      } 
+    /**
+     * Mutación para cambiar el estado del id.
+     * @param {estado} state 
+     * @param {Number} id 
+     */
+    SET_ID(state, id) {
+      state.id = id;
     },
-    actions: {
-        /**
-         * Función asincrónica para inicio de sesión y captura de token y del id del usuario.
-         * @param {store} context 
-         * @param {Objeto usuario} parametro 
-         */
-        logear: async function(context, parametro) {
-            var datos = {
-                nombreUsuario: parametro.nombreUsuario,
-                contrasena: parametro.contrasena
-            }
-            axios
-                .post("http://localhost:7070/login", datos, null)
-                .then(respuesta => {
-                    var token = respuesta.data.token.tokenJwt
-                    context.commit('SET_TOKEN', token)
-                    localStorage.setItem("tokenJwt", token);
-                    var id = respuesta.data.id;
-                    context.commit('SET_ID', id)
-                    localStorage.setItem("idUsuario", id);
-                })
-                .then(() => {
-                    router.push("logged");
-                })
-                .catch(() => alert("Credenciales incorrectas"));
-        },
-        /**
-         * Método para registrar un usuario.
-         * @param {store} context 
-         * @param {Objeto usuario} datos 
-         */
-        registrarUsuario: async function(context, datos) {
-            axios
-                .post("http://localhost:7070/registro", datos)
-                .then(() => {
-                    alert("Registrado con éxito");
-                    router.push("login");
-                })
-                .catch((error) => {
-                    alert(error.response.data.message);
-                });
-        },
-        logout: function(context) {
-            localStorage.setItem("tokenJwt", null);
-            localStorage.setItem("idUsuario", null);
-            context.commit('SET_TOKEN', null);
-            context.commit('SET_ID', null);
-            router.push('login');
-        }
+    /**
+     * Mutación para cambiar el estado del array de clientes.
+     * @param {estado} state 
+     * @param {Array obejtos cliente} clientes 
+     */
+    SET_CLIENTES(state, clientes) {
+      state.clientes = clientes;
     },
-    getters: {
-        /**
-         * Método para obtener el id.
-         * @param {Number} state 
-         */
-        getIdUsuario: function(state) {
-            return state.id;
-        },
-        /**
-         * 
-         * @param {String} state 
-         */
-        getToken: function(state) {
-            return state.token;
-        },
-        /**
-         * Método para saber si el usuario está autenticado.
-         * @param {store} state 
-         */
-        estaAutenticado: function(state) {
-            if (state.token != "null") {
-                return true;
-            } else {
-                return false;
-            }
-        }
+    /**
+      * Mutación para cambiar el estado del array de facturas.
+      * @param {estado} state 
+      * @param {Array obejtos factura} facturas 
+      */
+    SET_FACTURAS(state, facturas) {
+      state.facturas = facturas;
+    },
+    /**
+    * Mutación para cambiar el estado del contador.
+    * @param {estado} state 
+    * @param {Number} numero 
+    */
+    SET_CONTADOR(state, numero) {
+      state.contador = numero;
+    },
+    /**
+    * Mutación para cambiar el estado del array de clientes.
+    * @param {estado} state 
+    * @param {Objeto usuario} datos 
+    */
+    SET_DATOS_USUARIO(state, datos) {
+      state.datosUsuario = datos;
     }
+  },
+  actions: {
+    /**
+     * Función asincrónica para inicio de sesión y captura de token y del id del usuario.
+     * @param {store} context 
+     * @param {Objeto usuario} parametro 
+     */
+    logear: async function (context, parametro) {
+      var datos = {
+        nombreUsuario: parametro.nombreUsuario,
+        contrasena: parametro.contrasena
+      }
+      axios
+        .post("http://localhost:7070/login", datos, null)
+        .then(respuesta => {
+          var token = respuesta.data.token.tokenJwt
+          context.commit('SET_TOKEN', token)
+          localStorage.setItem("tokenJwt", token);
+          var id = respuesta.data.id;
+          context.commit('SET_ID', id)
+          localStorage.setItem("idUsuario", id);
+        })
+        .then(() => {
+          router.push("logged");
+        })
+        .catch(() => alert("Credenciales incorrectas"));
+    },
+    /**
+     * Método para registrar un usuario.
+     * @param {store} context 
+     * @param {Objeto usuario} datos 
+     */
+    registrarUsuario: async function (context, datos) {
+      axios
+        .post("http://localhost:7070/registro", datos)
+        .then(() => {
+          alert("Registrado con éxito");
+          router.push("login");
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    },
+    /**
+     * Función para destruir el token y la sesión del usuario.
+     * @param {store}
+     */
+    logout: function (context) {
+      localStorage.setItem("tokenJwt", null);
+      localStorage.setItem("idUsuario", null);
+      context.commit('SET_TOKEN', null);
+      context.commit('SET_ID', null);
+      router.push('login');
+    },
+    /**
+     * Función asincrónica para conectarse con la API y cargar los clientes del usuario.
+     * @param {store} context 
+     */
+    cargarClientes: async function (context) {
+      axios
+        .get(`http://localhost:7070/listar-clientes/${context.getters.getIdUsuario}`, {
+          headers: {
+            Authorization: "Bearer " + context.getters.getToken
+          }
+        })
+        .then(respuesta => {
+          context.commit('SET_CLIENTES', respuesta.data)
+        });
+    },
+  },
+  getters: {
+    /**
+     * Método para obtener el id.
+     * @param {Number} state 
+     */
+    getIdUsuario: function (state) {
+      return state.id;
+    },
+    /**
+     * 
+     * @param {String} state 
+     */
+    getToken: function (state) {
+      return state.token;
+    },
+    /**
+     * Método para saber si el usuario está autenticado.
+     * @param {store} state 
+     */
+    estaAutenticado: function (state) {
+      if (state.token != "null") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 })
