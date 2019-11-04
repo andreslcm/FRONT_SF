977d88509829a923fa47b3bc2e66ec77ad224a71
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2019-11-04 20:35:07 -0300

    estilos menu superior

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <div class="contenedor" v-if="this.$route.name === 'landing' || this.$route.name === 'registro' || this.$route.name === 'login'">
-      <div
-        class="menu">
-          <a href="/">Inicio</a>
-          <a href="login">Login</a>
-          <a href="registro">Registro</a>
-      </div>
+    <div>
+      <div class="contenedor" v-if="this.$route.name === 'landing' || this.$route.name === 'registro' || this.$route.name === 'login'">
+        <div
+          class="menu">
+            <a href="/">Inicio</a>
+            <a href="login">Login</a>
+            <a href="registro">Registro</a>
+        </div>
 
+      </div>
+      <router-view></router-view>
     </div>
-    <router-view></router-view>
   </div>
 </template>
 
@@ -18,27 +20,25 @@ export default {};
 </script>
 
 <style>
+*{
+  padding: 0%;
+  margin: 0%;
+  font-family:"Helvetica Neue", Helvetica;
+}
+
 .contenedor {
   display: grid;
   height: 7vh;
   grid-template-columns: repeat(8, 1fr);
-  grid-template-rows: 100%;
-
+  grid-template-rows: 100%; 
 }
 
 .menu {
   grid-column: 1/-1;
-  background-color: rgb(60, 63, 71);
+  background-color: #0C427D;
   display: flex;
   color: white;
   align-items: center;
-
-}
-
-.menu > a {
-  color: white;
-  margin-left: 13px;
-
 }
 
 </style>
