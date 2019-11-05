cde440ef75e8b1da6f029e0e107e7e74ee1514e2
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2019-11-04 21:08:30 -0300

    HTML landing

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-      
+    <div class="border">        
+    </div>
+    <div class="main">
+      <div class="padre">    
+        <div class="hijo1">    
+        <img class="foto" src="../assets/inicio_foto.png">
+        </div>
+        <div class="hijo2">               
+          <div>
+          <img class="imagen" src="../assets/logo-blanco.png">
+            <p>Gracias por usar Invoicing for Translator. La siguiente aplicación te perimitia hacer seguimiento del estatus de pago de cada registro, tener una base de datos de clientes, generar estadisticas financieras que sirvan de guia, entre otros aspectos. El programa proporciona los detalles y requerimientos para el uso correcto de la misma.</p>  
+            <br>                
+            <a href="/login" class="cta">
+              <span>Iniciar Sesion</span>
+              <svg width="13px" height="10px" viewBox="0 0 13 10" color="white">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </a>                
+            <a href="/registro" class="regis">¿No eres un usuario? Registrate!
+            </a>
+          </div>
+        </div>    
+      </div>
+    </div>  
   </div>
 </template>
 
@@ -10,6 +34,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
