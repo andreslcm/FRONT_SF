c13ebf8dc1018ece141839f4d2b34edad4db6176
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2020-02-04 17:09:41 -0300

    validacion de contraseña para +8caracteres, y alfanumerico, todos los campos deben estar compleatados para poder hacer un nuevo usuario

--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -2,35 +2,40 @@
 <div class="principal">
   <div class="log">
   <img class="imagen" src="../assets/logo-azul.png">
-    <ul>
-      <li>
-        <input v-model="usuario.nombre" type="text" id="nombre" placeholder="Nombre" title="Ingrese su Nombre" required/>
-      </li>
-      <li>
-        <input v-model="usuario.apellido" type="text" id="apellido" placeholder="Apellido" title="Ingrese su Apellido" required/>
-      </li>
-      <li>
-        <input v-model="usuario.contrasena" type="password" id="contrsena" placeholder="Contrseña" title="Ingrese su Contraseña" required/>
-      </li>
-      <li>
-        <input type="password" id="contrsena2" placeholder="Confirme su Contrseña" title="Confrime su Contraseña" required/>
-      </li>
-      <li>
-        <input v-model="usuario.nombreUsuario" type="text" id="nombreUsuario" placeholder="Nombre de usuario" title="Ingrese su Nombre de Usuario" required/>
-      </li>
-      <li>
-        <input v-model="usuario.correo" type="email" id="correo" placeholder="Correo electrónico" title="Ingrese su Correo" required/>
-      </li>
-      <div class="button_cont" align="center">
-        <button @click="registrarUsuario(usuario)" class="buttonhover" id="submit" target="_blank" rel="nofollow"><span>Registrarse</span></button>
-      </div>
-      <div class="inicioSesion">
-        <p>
-          ¿Ya tiene una cuenta?
-          <a href="/login">Inicie sesión</a>.
-        </p>
-      </div>
-    </ul>
+    <div @keyup="registro()">
+      <ul>
+        <li>
+          <input v-model="usuario.nombre" type="text" id="nombre" placeholder="Nombre" title="Ingrese su Nombre" required/>
+        </li>
+        <li>
+          <input v-model="usuario.apellido" type="text" id="apellido" placeholder="Apellido" title="Ingrese su Apellido" required/>
+        </li>
+        <li id="labelcont" class="labelcont">
+          <label for="contrsena">Debe contener 8 caracteres y al menos un numero</label>
+        </li>
+        <li>
+          <input v-model="usuario.contrasena" type="password" id="contrsena" placeholder="Contrseña" title="Ingrese su Contraseña" required/>
+        </li>
+        <li>
+          <input type="password" id="contrsena2" placeholder="Confirme su Contrseña" title="Confrime su Contraseña" required/>
+        </li>
+        <li>
+          <input v-model="usuario.nombreUsuario" type="text" id="nombreUsuario" placeholder="Nombre de usuario" title="Ingrese su Nombre de Usuario" required/>
+        </li>
+        <li>
+          <input v-model="usuario.correo" type="email" id="correo" placeholder="Correo electrónico" title="Ingrese su Correo" required/>
+        </li>
+        <div class="button_cont" align="center">
+          <button @click="registrarUsuario(usuario)" class="buttonhover" id="submit" target="_blank" rel="nofollow"><span>Registrarse</span></button>
+        </div>
+        <div class="inicioSesion">
+          <p>
+            ¿Ya tiene una cuenta?
+            <a href="/login">Inicie sesión</a>.
+          </p>
+        </div>
+      </ul>
+    </div>
   </div>
 </div>
 </template>
@@ -48,6 +53,53 @@ export default {
     };
   },
   methods: {
+    registro() {
+      //variables
+      var numeros = true;
+      var contrasena = document.getElementById("contrsena").value;
+      var contrasena2 = document.getElementById("contrsena2").value;
+      var nombre = document.getElementById("nombre").value;
+      var apellido = document.getElementById("apellido").value;
+      var nombreUsuario = document.getElementById("nombreUsuario").value;
+      var correo = document.getElementById("correo").value;
+
+      //si las contraseñas son iguales y distitnas de vacio
+      if (contrasena == contrasena2 && contrasena!=""){
+        var comparacion = true;
+      }
+      //si la contraseña es menor a 8 caracteres y no contiene numeros, muestra el label
+      if (contrasena.length <= 8) {
+        document.getElementById("labelcont").style.display = "block";
+      } else if (contrasena.search(/\d/) == -1) {
+        numeros = false;
+        document.getElementById("labelcont").style.display = "block";
+      }
+
+      //si la contraseña cumple con los requisitos, saca el label
+      if (contrasena.length >= 8) {
+        var longitud = true;
+        document.getElementById("labelcont").style.display = "none";
+      } else if (contrasena.search(/\d/) < -1) {
+        numeros = true;
+        document.getElementById("labelcont").style.display = "none";
+      }
+
+      //permite mostrar el boton de registro una vez que se cumolen con los requisitos
+      if (comparacion && longitud && nombre != "" && apellido != "" && nombreUsuario != "" && correo != ""){
+        document.getElementById("submit").style.display = "block";
+      }
+
+      //para sacar de vista el label
+      if (longitud && numeros){
+        document.getElementById("labelcont").style.display = "none";
+      }
+
+      //si la contraseña es modificada y no es mayor o igual a 8 caracteres y no contiene numeros,
+      //esconde el boton de registro
+      if (!comparacion || !longitud || nombre == "" || apellido == "" || nombreUsuario == "" || correo == ""){
+        document.getElementById("submit").style.display = "none";
+      }
+    },
     ...mapActions(["registrarUsuario"])
   }
 };
@@ -57,6 +109,13 @@ export default {
 *{
   font-family:"Helvetica Neue", Helvetica;
   overflow: hidden;
+}
+.labelcont{
+    display: none;
+    margin-left: 5%;
+}
+#submit{
+    display: none;
 }
 img{
   padding: 10%;
